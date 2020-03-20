2dd4598dfb3fef9da5d92f81dea83d6a54019720
Ming Hong <51311701+Mingyou0426@users.noreply.github.com>
2020-03-21 01:32:40 +0700

    Add files via upload

--- analyzer_gui.py
+++ analyzer_gui.py
@@ -98,82 +98,82 @@ fig1 = go.Figure(data=[go.Bar(
             textposition='auto',
         )], layout=layout)
 fig1.show()
-print('\n')
-print('2. Price attribute analysis(Added)')
-print('\n')
-print('The score of the price which is calculated all of the sum of each price score and then divided by the number of the price products')
-print('\nScore of prices')
-# average of score for price
-# wine_csv["price"].fillna(0, inplace = True) 
-price_csv = wine_csv.groupby('price')['score'].mean()
-dic = price_csv.to_dict()
-price = list(dic.keys())
-score = list(dic.values())
+# print('\n')
+# print('2. Price attribute analysis(Added)')
+# print('\n')
+# print('The score of the price which is calculated all of the sum of each price score and then divided by the number of the price products')
+# print('\nScore of prices')
+# # average of score for price
+# # wine_csv["price"].fillna(0, inplace = True) 
+# price_csv = wine_csv.groupby('price')['score'].mean()
+# dic = price_csv.to_dict()
+# price = list(dic.keys())
+# score = list(dic.values())
 
-for i in range(len(score)):
-    val = round(score[i],2)
-    score[i] = val    
+# for i in range(len(score)):
+#     val = round(score[i],2)
+#     score[i] = val    
 
-price1 = {'price': price, 'score': score}
-df = pd.DataFrame(price1)
-# print(df)
-fig = go.Figure(data=[go.Table(
-    header=dict(values=df.columns,
-                fill_color='paleturquoise',
-                align='left'),
-    cells=dict(values=[df.price, df.score],
-               fill_color='lavender',
-               align='left'))
-])
-fig.show()
+# price1 = {'price': price, 'score': score}
+# df = pd.DataFrame(price1)
+# # print(df)
+# fig = go.Figure(data=[go.Table(
+#     header=dict(values=df.columns,
+#                 fill_color='paleturquoise',
+#                 align='left'),
+#     cells=dict(values=[df.price, df.score],
+#                fill_color='lavender',
+#                align='left'))
+# ])
+# fig.show()
 
-# print('Top 10 of the prices')
-layout = go.Layout(
-    title=go.layout.Title(
-        text='Top 10 of the prices'
-    ),
-    xaxis=go.layout.XAxis(
-        title=go.layout.xaxis.Title(
-            text='Prices',
-            font=dict(
-                family='Courier New, monospace',
-                size=18,
-                color='#7f7f7f'
-            )
-        )
-    ),
-    yaxis=go.layout.YAxis(
-        title=go.layout.yaxis.Title(
-            text='Score',
-            font=dict(
-                family='Courier New, monospace',
-                size=18,
-                color='#7f7f7f'
-            )
-        )
-    )
-)
-price_csv4view = price_csv.sort_values(ascending=False).head(10)
-dic = price_csv4view.to_dict()
-price = list(dic.keys())
-score = list(dic.values())
+# # print('Top 10 of the prices')
+# layout = go.Layout(
+#     title=go.layout.Title(
+#         text='Top 10 of the prices'
+#     ),
+#     xaxis=go.layout.XAxis(
+#         title=go.layout.xaxis.Title(
+#             text='Prices',
+#             font=dict(
+#                 family='Courier New, monospace',
+#                 size=18,
+#                 color='#7f7f7f'
+#             )
+#         )
+#     ),
+#     yaxis=go.layout.YAxis(
+#         title=go.layout.yaxis.Title(
+#             text='Score',
+#             font=dict(
+#                 family='Courier New, monospace',
+#                 size=18,
+#                 color='#7f7f7f'
+#             )
+#         )
+#     )
+# )
+# price_csv4view = price_csv.sort_values(ascending=False).head(10)
+# dic = price_csv4view.to_dict()
+# price = list(dic.keys())
+# score = list(dic.values())
 
-for i in range(len(score)):
-    val = round(score[i],2)
-    score[i] = val    
+# for i in range(len(score)):
+#     val = round(score[i],2)
+#     score[i] = val    
 
-price1 = {'price': price, 'score': score}
-df = pd.DataFrame(price1)
+# price1 = {'price': price, 'score': score}
+# df = pd.DataFrame(price1)
 
-fig1 = go.Figure(data=[go.Bar(
-            x=price, y=score,
-            text=score,
-            textposition='auto',
-        )], layout=layout)
-fig1.show()
+# fig1 = go.Figure(data=[go.Bar(
+#             x=price, y=score,
+#             text=score,
+#             textposition='auto',
+#         )], layout=layout)
+# fig1.show()
 
 print('\n\n')
-print('3. Price attribute analysis\n')
+print('2. Price attribute analysis\n')
 print('Price_score is defined based on the price and "Null" is elminated\n')
 print('Price_score criteria')
 
@@ -288,7 +288,7 @@ print('Kind of total counts of price: ', wine_csv.shape[0])
 print('\n Null: ', nullcnt)
 
 print('\n')
-print('4. Variety attribute analysis')
+print('3. Variety attribute analysis')
 print('\n')
 print('The score of the variety which is calculated all of the sum of each varieties score and then divided by the number of the varieties products')
 print('\nIf data is Null it is given average score')
@@ -362,38 +362,79 @@ fig1 = go.Figure(data=[go.Bar(
         )], layout=layout)
 fig1.show()
 
-# fig.show()
-# average of score for variety
-# variety_csv = wine_csv.groupby('variety')['score'].mean()
-# pd.set_option('display.max_rows', variety_csv.shape[0]+1)
-# print(variety_csv)
-# print('\n\n')
-# # preprocess for state chart
-# plt.figure()
-# # variety chart
-# variety_csv.plot.bar(x='variety',y='score',rot=0,figsize=(20,10))
+total_scores = []
+for ind in range(0, wine_csv.shape[0]):
+    totscore = 0
+    totscore = wine_csv.loc[ind]['score']*3.5
+    if wine_csv.loc[ind]['price'] is not None:
+        if wine_csv.loc[ind]['price'] > 250:
+            totscore = totscore + 1 * 3
+        elif wine_csv.loc[ind]['price'] > 180:
+            totscore = totscore + 2 * 3
+        elif wine_csv.loc[ind]['price'] > 140:
+            totscore = totscore + 3 * 3
+        elif wine_csv.loc[ind]['price'] > 120:
+            totscore = totscore + 4 * 3
+        elif wine_csv.loc[ind]['price'] > 100:
+            totscore = totscore + 5 * 3
+        elif wine_csv.loc[ind]['price'] > 80:
+            totscore = totscore + 6 * 3
+        elif wine_csv.loc[ind]['price'] > 60:
+            totscore = totscore + 7 * 3
+        elif wine_csv.loc[ind]['price'] > 40:
+            totscore = totscore + 8 * 3
+        elif wine_csv.loc[ind]['price'] > 20:
+            totscore = totscore + 9 * 3
+        elif wine_csv.loc[ind]['price'] > 0:
+            totscore = totscore + 10 * 3
+    if wine_csv.loc[ind]['state'] is not None:
+        totscore = totscore + state_csv.loc[wine_csv.loc[ind]['state']]*1.5
+    if wine_csv.loc[ind]['variety'] is not None:
+        if pd.isnull(wine_csv.loc[ind]['variety'])==False:
+            totscore = totscore + variety_csv.loc[wine_csv.loc[ind]['variety']]*2
+    totscore = round(totscore/10,2)
+    total_scores.append(totscore)
 
-# total_scores = []
-# for ind in range(0, wine_csv.shape[0]):
-#     totscore = 0
-#     totscore = wine_csv.loc[ind]['score']*4
-#     if wine_csv.loc[ind]['price'] is not None:
-#         if wine_csv.loc[ind]['price'] > 300:
-#             totscore = totscore + 1 * 3
-#         elif wine_csv.loc[ind]['price'] > 230:
-#             totscore = totscore + 2 * 3
-#         elif wine_csv.loc[ind]['price'] > 100:
-#             totscore = totscore + 3 * 3
-#         elif wine_csv.loc[ind]['price'] > 50:
-#             totscore = totscore + 4 * 3
-#         else:
-#             totscore = totscore + 5 * 3
-#     if wine_csv.loc[ind]['state'] is not None:
-#         totscore = totscore + state_csv.loc[wine_csv.loc[ind]['state']]*1.5
-#     if wine_csv.loc[ind]['variety'] is not None:
-#         if pd.isnull(wine_csv.loc[ind]['variety'])==False:
-#             totscore = totscore + variety_csv.loc[wine_csv.loc[ind]['variety']]*1.5
-#     total_scores.append(totscore)
+wine_csv['totalscore'] = total_scores
+fig = go.Figure(data=[go.Table(
+    header=dict(values=list(wine_csv.columns),
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[wine_csv.id, wine_csv.country, wine_csv.title, wine_csv.score, wine_csv.price, wine_csv.state, wine_csv.region_1, wine_csv.region_2, wine_csv.variety, wine_csv.winery, wine_csv.totalscore],
+               fill_color='lavender',
+               align='left'))
+])
+fig.show()
 
-# wine_csv['totalscore'] = total_scores
-# print(wine_csv)
+wine_csv4view = wine_csv.sort_values(by='totalscore', ascending=False).head(10)
+layout = go.Layout(
+    title=go.layout.Title(
+        text='Top 10 of the total score'
+    ),
+    xaxis=go.layout.XAxis(
+        title=go.layout.xaxis.Title(
+            text='Title',
+            font=dict(
+                family='Courier New, monospace',
+                size=18,
+                color='#7f7f7f'
+            )
+        )
+    ),
+    yaxis=go.layout.YAxis(
+        title=go.layout.yaxis.Title(
+            text='Total Score',
+            font=dict(
+                family='Courier New, monospace',
+                size=18,
+                color='#7f7f7f'
+            )
+        )
+    )
+)
+fig1 = go.Figure(data=[go.Bar(
+            x=wine_csv4view.title, y=wine_csv4view.totalscore,
+            text=wine_csv4view.totalscore,
+            textposition='auto',
+        )], layout=layout)
+fig1.show()
